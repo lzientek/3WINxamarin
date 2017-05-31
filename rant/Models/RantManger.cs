a28a0331c0b5a22209b4f0324a91af27bfd7fac0
lzientek <lucas@zientek.fr>
2017-05-31 15:28:57 +0200

    Rant app

---
+++ rant/Models/RantManger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace rant.Models
+{
+    public class RantManger
+    {
+        public RantManger()
+        {
+        }
+    }
+}
---
+++ rant/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace rant
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
